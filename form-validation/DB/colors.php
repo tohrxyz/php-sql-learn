3ace3697123ffd81ebfa90218c59b9312da8d300
tomashrib <tomas.hrib@proton.me>
2023-02-20 17:03:46 +0100

    added some prerequisite files to form-validation

---
+++ form-validation/DB/colors.php
@@ -0,0 +1,16 @@
+<?php
+    $colors = [
+        [
+            "id" => 1,
+            "name" => "cierna",
+        ],
+        [
+            "id" => 2,
+            "name" => "zelena",
+        ],
+        [
+            "id" => 3,
+            "name" => "modra",
+        ],
+    ];
+?>
---
+++ form-validation/DB/genders.php
@@ -0,0 +1,16 @@
+<?php
+    $genders = [
+        [
+            "id" => 1,
+            "name" => "muz",
+        ],
+        [
+            "id" => 2,
+            "name" => "zena",
+        ],
+        [
+            "id" => 3,
+            "name" => "helikoptera",
+        ],
+    ];
+?>
