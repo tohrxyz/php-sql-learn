8d325053314c71527cedeb8655bfbd58a17d7bda
tomashrib <tomas.hrib@proton.me>
2023-04-04 08:49:14 +0200

    feat: clean user input preventing sql injection

--- mysql-php-integration/insert.php
+++ mysql-php-integration/insert.php
@@ -9,13 +9,21 @@
     $sql = "INSERT INTO
                 persons
             SET
-                fname='$person[fname]',
-                lname='$person[lname]',
-                age='$person[age]'
+                fname=?,
+                lname=?,
+                age=?
     ";
 
     $stmt = mysqli_prepare($_DB['conn'], $sql); // prikaz posli na konkretnu ciernu obrazovku
-    // mysqli_stmt_bind_param($stmt, "i", $id); // osetri vstupy
+    
+    // bind parameters for markers
+    // and cleans the data from the user input (SQL Injection)
+    mysqli_stmt_bind_param(
+        $stmt, "ssi",  // string, string, integer
+        $person["fname"], 
+        $person["lname"],
+        $person["age"]
+    );
     
     mysqli_stmt_execute($stmt); // stlac enter a prikaz vykonaj
     $res = mysqli_stmt_get_result($stmt); // vysledok/suhrn prikazu uloz do premennej
