a252b0f2c63cf4434b07062331c27ece2fd8106b
tomashrib <tomas.hrib@proton.me>
2023-04-04 09:28:31 +0200

    feat: change to POST and validate form

--- mysql-php-integration/insert.php
+++ mysql-php-integration/insert.php
@@ -3,33 +3,38 @@
     require_once "Libs/Helper.php";
     require_once "Libs/DB.php";
 
-    // http://localhost/php-sql-learn/mysql-php-integration/insert.php?fname=Tomas&lname=Mastalir&age=39
-    $person = $_GET;
+    $person = $_POST;
 
-    $sql = "INSERT INTO
-                persons
-            SET
-                fname=?,
-                lname=?,
-                age=?
-    ";
+    $error = [
+        "fname" => false,
+        "lname" => false,
+        "age" => false,
+    ];
 
-    $stmt = mysqli_prepare($_DB['conn'], $sql); // prikaz posli na konkretnu ciernu obrazovku
+    if(count($_POST) != 0){
+        if(trim($_POST["fname"]) == ""){
+            $error["fname"] = true;
+        }
+        if(trim($_POST["lname"]) == ""){
+            $error["lname"] = true;
+        }
+        if(trim($_POST["age"]) == ""){
+            $error["age"] = true;
+        }
+        if(!$error["fname"] && !$error["lname"] && !$error["age"]){
+            $sql = "INSERT INTO persons SET fname=?, lname=?, age=?";
+            $stmt = mysqli_prepare($_DB['conn'], $sql); 
+            mysqli_stmt_bind_param($stmt, "ssi", $person["fname"], $person["lname"], $person["age"]); 
+            
+            mysqli_stmt_execute($stmt); 
+            $res = mysqli_stmt_get_result($stmt); 
+            
+            mysqli_stmt_close($stmt); 
+            mysqli_close($_DB["conn"]); 
+        }
+    }
+
     
-    // bind parameters for markers
-    // and cleans the data from the user input (SQL Injection)
-    mysqli_stmt_bind_param(
-        $stmt, "ssi",  // string, string, integer
-        $person["fname"], 
-        $person["lname"],
-        $person["age"]
-    );
-    
-    mysqli_stmt_execute($stmt); // stlac enter a prikaz vykonaj
-    $res = mysqli_stmt_get_result($stmt); // vysledok/suhrn prikazu uloz do premennej
-    
-    mysqli_stmt_close($stmt); // ako keby som stlacil v ciernej obrazovke CTRL + C
-    mysqli_close($_DB["conn"]); // zavriem okno s ciernou obrazovkou
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,16 +42,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert</title>
+    <style>
+        .error-border{
+            border-color: red;
+        }
+    </style>
 </head>
 <body>
     <?php require_once "Components/HeaderApp.php" ?>
     <h1>Insert</h1>
 
-    <form action="">
-        <input type="text" name="fname" placeholder="First name">
-        <input type="text" name="lname" placeholder="Last name">
-        <input type="number" name="age" placeholder="Age">
-        <input type="submit" value="Insert">
+    <?php if($error["fname"] || $error["lname"] || $error["age"]):?>
+        <div class="alert">Formular je zle vyplneny!</div>
+    <?php endif ?>
+
+    <form action="" method="POST">
+        <input type="text" name="fname" placeholder="Meno" value="<?= $person["fname"]?>" 
+        class="<?php if($error["fname"]): ?> error-border <?php endif ?>">
+        <input type="text" name="lname" placeholder="Priezvisko" value="<?= $person["lname"]?>"
+        class="<?php if($error["lname"]): ?> error-border <?php endif ?>">
+        <input type="text" name="age" placeholder="Vek" value="<?= $person["age"]?>"
+        class="<?php if($error["age"]): ?> error-border <?php endif ?>">
+        <button>Ulozit</button>
     </form>
 </body>
 </html>
