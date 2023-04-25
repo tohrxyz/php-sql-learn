306696500b3cf6749f3ffb9e9714b435c8ded5c3
tomashrib <tomas.hrib@proton.me>
2023-04-25 09:19:00 +0200

    fix: blank page when insert/update without id in get url parameter

--- web-sql/person-form.php
+++ web-sql/person-form.php
@@ -1,5 +1,8 @@
 <?php
+    ini_set('display_errors',"1");
     error_reporting(E_ERROR);
+    
+
     require_once "Libs/Helper.php";
     require_once "Libs/DB.php";
     
@@ -7,47 +10,50 @@
     $message = "";
     $person = array(); // Initialize an empty array for the person variable
 
-    if (!isset($_DB['conn'])) {
-        $message = "Error: Unable to establish a database connection.";
-    } else { 
-        if ($id) {
-            $sql = "SELECT * FROM persons WHERE id=?";
-            $stmt = mysqli_prepare($_DB['conn'], $sql);
-            mysqli_stmt_bind_param($stmt, "i", $id);
-            
-            mysqli_stmt_execute($stmt);
-            $res = mysqli_stmt_get_result($stmt);
-            
-            $person = mysqli_fetch_assoc($res);
-            debug($person);
-        } 
+    if ($id) {
+        $sql = "SELECT * FROM persons WHERE id=?";
+        $stmt = mysqli_prepare($_DB['conn'], $sql);
+        mysqli_stmt_bind_param($stmt, "i", $id);
         
-        if(count($_POST) > 0) {
-            $person = $_POST;
-            if($id) {
-                $sql = "UPDATE persons SET fname=?, lname=?, age=? WHERE id=?";
-                $stmt = mysqli_prepare($_DB['conn'], $sql);
-                mysqli_stmt_bind_param($stmt, "ssii", $person["fname"], $person["lname"], $person["age"], $id);
-                mysqli_stmt_execute($stmt);
-                $message = "Record updated successfully";
-            } else {
-                $sql = "INSERT INTO
-                            persons
-                        SET
-                            fname=?,
-                            lname=?,
-                            age=?"
-                ;
-                $stmt = mysqli_prepare($_DB['conn'], $sql);
-                mysqli_stmt_bind_param($stmt, "ssi", $person["fname"], $person["lname"], $person["age"]);
-                mysqli_stmt_execute($stmt);
-                $message = "Record inserted successfully";
-            }
-        } 
-            
+        mysqli_stmt_execute($stmt);
+        $res = mysqli_stmt_get_result($stmt);
+        
+        $person = mysqli_fetch_assoc($res);
+        debug($person);
+    } 
+    
+    if(count($_POST) > 0) {
+        $person = $_POST;
+        if($id) {
+            # update record
+            $sql = "UPDATE
+                        persons
+                    SET
+                        fname=?,
+                        lname=?,
+                        age=?
+                    WHERE
+                        id=?
+                    ";
+            $stmt = mysqli_prepare($_DB['conn'], $sql);
+            mysqli_stmt_bind_param($stmt, "ssii", $person["fname"], $person["lname"], $person["age"], $id);
+            mysqli_stmt_execute($stmt);
+            $message = "Record updated successfully";
+
+        } else {
+            # insert new record if id is not set
+            $sql = "INSERT INTO persons (fname, lname, age) VALUES (?, ?, ?)";
+            $stmt = mysqli_prepare($_DB['conn'], $sql);
+            mysqli_stmt_bind_param($stmt, "ssi", $person["fname"], $person["lname"], $person["age"]);
+            mysqli_stmt_execute($stmt);
+            $message = "Record inserted successfully";
+        }
+    } 
+        
+    if ($stmt !== null) {
         mysqli_stmt_close($stmt);
-        mysqli_close($_DB["conn"]);
     }
+    mysqli_close($_DB["conn"]);
 
 ?>
 
